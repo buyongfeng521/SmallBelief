5b4c42c2742e18817754b42f28c0862035d29c1c
buyongfeng521 <309929800@qq.com>
2016-06-05 19:32:32 +0800

    feat:order & user & user Modal

---
+++ OperationManager/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OperationManager.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        #region 购物车
+        public ActionResult CartList()
+        {
+            return View();
+        } 
+        #endregion
+
+
+
+        #region 订单
+        public ActionResult OrderList()
+        {
+            return View();
+        } 
+        #endregion
+
+	}
+}
---
+++ OperationManager/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OperationManager.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public ActionResult UserList()
+        {
+            return View();
+        }
+
+	}
+}
