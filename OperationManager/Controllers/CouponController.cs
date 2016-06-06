3d73ff8af954d320e30ad5cf253b44a1effe280b
buyongfeng521 <309929800@qq.com>
2016-06-06 18:01:21 +0800

    feat:coupon modal add

---
+++ OperationManager/Controllers/CouponController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OperationManager.Controllers
+{
+    public class CouponController : Controller
+    {
+        //
+        // GET: /Coupon/
+        public ActionResult CouponList()
+        {
+            return View();
+        }
+	}
+}
