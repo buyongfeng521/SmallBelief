5e166d1dd0c026798f2d4966c3c4450bf85e31f8
buyongfeng521 <309929800@qq.com>
2016-07-18 17:59:14 +0800

    feat:DTOHelper add CartDTO

--- HelperCommon/DTOHelper.cs
+++ HelperCommon/DTOHelper.cs
@@ -31,6 +31,7 @@ namespace HelperCommon
                 .ForMember(desc => desc.pay_time, opt => opt.MapFrom(src => src.pay_time == null ? "" : ((DateTime)src.pay_time).ToString("yyyy-MM-dd HH:mm:ss")))
                 .ForMember(desc => desc.shipping_time, opt => opt.MapFrom(src => src.shipping_time == null? "": ((DateTime)src.shipping_time).ToString("yyyy-MM-dd HH:mm:ss")));
             Mapper.CreateMap<t_order_goods, OrderGoodsDTO>();
+            Mapper.CreateMap<t_cart, CartDTO>().ForMember(desc => desc.goods_img, opt => opt.MapFrom(src => src.t_goods.goods_img));
 
 
         }
