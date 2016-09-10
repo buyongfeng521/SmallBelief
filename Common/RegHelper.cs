ce5f653eb31eea35859303acc77e61ca46d61d3d
buyongfeng521 <849001270@qq.com>
2016-09-10 09:46:12 +0800

    fix:184 mobile phone reg

--- Common/RegHelper.cs
+++ Common/RegHelper.cs
@@ -25,7 +25,7 @@ namespace Common
                 //联通
                 Regex lReg = new Regex(@"^1[34578][01256]\d{8}$");
                 //移动
-                Regex yReg = new Regex(@"^(134[012345678]\d{7}|1[34578][012356789]\d{8})$");
+                Regex yReg = new Regex(@"^(134[012345678]\d{7}|1[34578][0123456789]\d{8})$");
                 if(dReg.IsMatch(phone) || lReg.IsMatch(phone) || yReg.IsMatch(phone))
                 {
                     result = true;
