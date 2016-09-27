8c80cde2608872a1f9421d3b21b1a3227e94a010
buyongfeng521 <849001270@qq.com>
2016-09-27 14:48:12 +0800

    feat:add android version

---
+++ Model/DTOModel/VersionDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DTOModel
+{
+    public class VersionDTO
+    {
+        public int version { get; set; }
+        public int version_lowest { get; set; }
+        public string update_content { get; set; }
+        public string update_url { get; set; }
+
+    }
+}
---
+++ WebApi/Controllers/SettingsController.cs
@@ -0,0 +1,39 @@
+﻿using Model.DTOModel;
+using Model.FormatModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class SettingsController : ApiController
+    {
+        /// <summary>
+        /// 安卓版本信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public RetInfo<VersionDTO> VersionInfo()
+        {
+            RetInfo<VersionDTO> ret = new RetInfo<VersionDTO>();
+            try
+            {
+                VersionDTO dto = new VersionDTO();
+                dto.version = 110;//1.1.0
+                dto.version_lowest = 110; //1.1.0
+                dto.update_content = "";
+                dto.update_url = "http://oaadhsucq.bkt.clouddn.com/ZLZH.apk";
+                ret.status = true;
+                ret.Data = dto;
+            }
+            catch (Exception ex)
+            {
+                ret.msg = ex.ToString();
+            }
+            return ret;
+        }
+    }
+}
